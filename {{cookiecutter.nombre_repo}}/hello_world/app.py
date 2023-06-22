b4154c851becba78039da1181b0968adb9102566
Ricardo Arian Ortega Garcia <40581565+rortega-sps@users.noreply.github.com>
2023-06-22 07:43:00 -0700

    feat: se habilita consulta a servicio de ip

--- {{cookiecutter.nombre_repo}}/hello_world/app.py
+++ {{cookiecutter.nombre_repo}}/hello_world/app.py
@@ -25,13 +25,12 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+     try:
+         ip = requests.get("http://checkip.amazonaws.com/")
+     except requests.RequestException as e:
+         # Send some context about this error to Lambda Logs
+         print(e)
+         raise e
 
     return {
         "statusCode": 200,
